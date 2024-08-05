5d8573a748ede4be2dab6e3b25aa65ea9d73bda1
Juli Astuti <juliastutinpt@gmail.com>
2024-08-05 11:25:58 +0800

    revise run all cases

--- spec/case4.js
+++ spec/case4.js
@@ -14,26 +14,40 @@ async function runCase4() {
     await welcomePage.waitForWelcomePage(driver);
     console.log("Welcome page is displayed");
 
-    // Check image 1 is displayed
-    const image = await driver.$('android=new UiSelector().resourceId("jp.co.i_bec.suteki_happy:id/img_login_tutorial_01")');
-    await driver.waitUntil(
-      async () => {
-        try {
-          const image1 = await driver.$(image);
-          return await image1.isDisplayed();
-        } catch (error) {
-          return false;
-        }
-      },
-      {
-        timeout: 60000,
-        interval: 1000,
-        timeoutMsg: "Image 1 is not displayed within the timeout period",
-      }
-    );
+    // Check if any image is displayed
+    const images = [
+      'android=new UiSelector().resourceId("jp.co.i_bec.suteki_happy:id/img_login_tutorial_01")'
+      // 'android=new UiSelector().resourceId("jp.co.i_bec.suteki_happy:id/img_login_tutorial_02")',
+      // 'android=new UiSelector().resourceId("jp.co.i_bec.suteki_happy:id/img_login_tutorial_03")',
+    ];
 
-    console.log("Image 1 is displayed");
-  
+    async function checkImages() {
+      let foundImages = 0;
+      const maxRetries = 2;
+      let retries = 0;
+
+      while (foundImages < images.length && retries < maxRetries) {
+        for (const image of images) {
+          try {
+            await driver.$(image);
+            foundImages++;
+            console.log(`Image ${foundImages} displayed`);
+          } catch (error) {
+            console.error(`Error finding image ${image}:`, error);
+          }
+
+          if (foundImages === images.length) {
+            console.log("All images displayed");
+            return;
+          }
+        }
+        retries++;
+        console.log(`Waiting for remaining images (retry: ${retries})`);
+        await driver.pause(4000);
+      }
+      throw new Error(`Not all images are found after ${retries} retries`);
+    }
+    await checkImages();
   } catch (error) {
     console.error("Error occurred in runCase4:", error);
   } finally {
